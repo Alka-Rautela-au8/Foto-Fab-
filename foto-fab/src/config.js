d230eb80df73858e1936c568aac58884422b63ee
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-08-19 17:29:44 +0530

    access_key added

--- foto-fab/src/config.js
+++ foto-fab/src/config.js
@@ -1,21 +1,9 @@
 export const key = {
-  ACCESS_KEY: "gR1gK6qumpkKPVT-nuV9NjqYqZTcE3LC8k7D2IS_aZc",
-  SECRET_KEY: "RWspsOH54UmdqpzQ_hZaO2ngH530i7YqKltmZTnoVBQ",
-  REDIRECT_URI: "http://localhost:3000/",
+  ACCESS_KEY: "TMg9F75c8C2pWrfLfAkp3eHjuP8Rs-S3Dq6IAATLKWU",
+  SECRET_KEY: "1xssBFyjPq0-F5xH_AZyRzOHiDUj-1URXyzG3exBMN8",
+  REDIRECT_URI: "https://foto-fab.herokuapp.com/",
 };
 
-//-----------APP-1------------------------
-//access_key 1: "bxVBcSKbVrcezdSKMdwn8SZmZ29H-p5AYcCwLo69llI",
-//secret_key 1: "y3_AmDNlFOnVsYY-IjoM9DS5DsaUmpguGbM4-kD-I9k"
-
-//-----------APP-2-------------------------
-//access_key 2: gR1gK6qumpkKPVT-nuV9NjqYqZTcE3LC8k7D2IS_aZc
-//secret_key 2: RWspsOH54UmdqpzQ_hZaO2ngH530i7YqKltmZTnoVBQ
-
-//-----------Foto-fab----------------------
-//access_key : "FnpgKhsrpu43nWAdiMJxA3Rj0W1Yo6aPOTdDFINzgsg"
-//secret_key : "Qs5JTqlQBJT6z8HH2k7vyXzs4XV7V46_4KbTTH3WOK0"
-
-//------------FOTO-FAB---------------------
-//access_key: "AjpwwYKydi1dZXv_5HEmAARHjC5kaj_4rDzVs1rhsZM"
-//secret_key: "B30_ee5RRX00ilVo0uP7871yZkthAza8WwatDvDhhBY"
+//-----------Heroku-1------------------------
+//access_key 1: "TMg9F75c8C2pWrfLfAkp3eHjuP8Rs-S3Dq6IAATLKWU",
+//secret_key 1: "1xssBFyjPq0-F5xH_AZyRzOHiDUj-1URXyzG3exBMN8"
